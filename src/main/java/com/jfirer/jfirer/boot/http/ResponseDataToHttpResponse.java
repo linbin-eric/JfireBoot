4cb6d3e60335d4b27b595b391720abb4231e502a
linbin <495561397@qq.com>
2023-09-27 17:31:12 +0800

    修改CORS策略

--- src/main/java/com/jfirer/jfirer/boot/http/ResponseDataToHttpResponse.java
+++ src/main/java/com/jfirer/jfirer/boot/http/ResponseDataToHttpResponse.java
@@ -25,7 +25,8 @@ public class ResponseDataToHttpResponse implements WriteProcessor<Object>
         response.getHeaders().put("allow", "GET,PUT,POST,HEAD");
         response.getHeaders().put("access-control-allow-methods", "GET,PUT,POST,HEAD");
         response.getHeaders().put("Access-Control-Max-Age", "86400");
-        response.getHeaders().put("Access-Control-Allow-Headers", "authorization,Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
+//        response.getHeaders().put("Access-Control-Allow-Headers", "authorization,Authorization,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
+        response.getHeaders().put("Access-Control-Allow-Headers", "*");
         next.fireWrite(response);
     }
 }
