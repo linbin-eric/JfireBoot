5f65e9791d3fd28b924fafafa6d9d91949dc2447
linbin <495561397@qq.com>
2024-11-03 16:09:46 +0800

    代理服务器相关代码调整完毕

--- src/main/java/com/jfirer/jfirer/boot/netServer/TransferProcessor.java
+++ src/main/java/com/jfirer/jfirer/boot/netServer/TransferProcessor.java
@@ -1,16 +1,11 @@
 package com.jfirer.jfirer.boot.netServer;
 
-import com.jfirer.baseutil.StringUtil;
 import com.jfirer.jfirer.boot.netServer.config.ResourceConfig;
 import com.jfirer.jfirer.boot.netServer.config.ResourceHandler;
-import com.jfirer.jfirer.boot.netServer.impl.FileHandler;
-import com.jfirer.jfirer.boot.netServer.impl.ProxyHandler;
 import com.jfirer.jnet.common.api.ReadProcessor;
 import com.jfirer.jnet.common.api.ReadProcessorNode;
 import com.jfirer.jnet.extend.http.decode.HttpRequest;
 import com.jfirer.jnet.extend.http.decode.HttpResponse;
-import lombok.Builder;
-import lombok.Data;
 
 import java.util.Arrays;
 
